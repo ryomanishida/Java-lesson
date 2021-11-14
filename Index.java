2591e552bd7754f5b5c54647c72a0dbeb255b1b6
works <nishida-w@ryoma-mac.local>
2021-11-14 18:35:06 +0900

    実行

---
+++ Index.java
@@ -0,0 +1,17 @@
+class Index {
+  public static void main(String[] args) {
+
+    String name = "ryoma";
+    int age = 24;
+    double height = 1.2;
+    double weight = 55;
+    double bmi = weight / height / height;
+
+
+    System.out.println("名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです" );
+    System.out.println("体重は" + weight + "kgです");
+    System.out.println("BMIは" + bmi + "です");
+  }
+}
