e2f31b5e9afabed25fbc9448ee328318df5f11cd
works <nishida-w@ryoma-mac.local>
2021-11-15 20:16:39 +0900

    switch文

--- Index.java
+++ Index.java
@@ -13,5 +13,21 @@ class Index {
     System.out.println("身長は" + height + "mです" );
     System.out.println("体重は" + weight + "kgです");
     System.out.println("BMIは" + bmi + "です");
+
+
+
+
+
+    switch (age % 5){
+      case 0:
+        System.out.println(age + "周年です");
+        break;
+      case 3:
+        System.out.println("再来年" + (age + 2));
+        break;
+      case 4:
+        System.out.println("来年" + (age + 1));
+        break;
+    }
   }
 }
