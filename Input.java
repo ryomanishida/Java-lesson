2b576bd62497eb1ad2337ff6853f508c3873ec87
ryomanishida <nriyoma@gmail.com>
2022-01-09 22:15:38 +0900

    Input:3-6

--- Input.java
+++ Input.java
@@ -11,9 +11,23 @@ class Input {
         // System.out.println(x);
 
         // 3,int 型の変数 x に数値を入力し、x を 1 乗、2 乗、3 乗した結果を表示するプログラムを作成しなさい。
-        int x = Integer.parseInt(br.readLine());
-        System.out.println(x);
-        System.out.println(x * x);
+        // int x = Integer.parseInt(br.readLine());
+        // System.out.println(x);
+        // System.out.println(x * x);
+
+        // 4,int 型の変数 x、y にそれぞれ数値を入力し、x と y の和、差（x－y）、積、商と余り(x÷y)、を表示するプログラムを作成しなさい。
+        // int x = Integer.parseInt(br.readLine());
+        // int y = Integer.parseInt(br.readLine());
+        // System.out.println(x - y);
+        // System.out.println(x / y);
+        // System.out.println((x + y) / 2);
+
+
+        // 6,年齢を訊ね、生まれてから現在までの、おおよその日数を表示するプログラムを作成しなさい。
+        // ※ 	日数の計算は閏年等を考慮せず、年齢×365とする。
+        // System.out.println("何歳ですか？");
+        // int a = Integer.parseInt(br.readLine());
+        // System.out.println(a * 365);
 
     }
 }
