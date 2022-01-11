e8507e2de11bb191b1c4b9f1aedc6c9c1ac1dcde
ryomanishida <nriyoma@gmail.com>
2022-01-11 20:13:39 +0900

    Bif:13

--- Bifurcation.java
+++ Bifurcation.java
@@ -195,11 +195,80 @@ class Bifurcation {
     // }
 
 
-    // 12,好きな鮨（すし）を選択させ、それに対応したメッセージを表示する、鮨占いプログラムを作成しなさい。*switchを使う
-
-
     // 13,月を表す数値を入力し、その月の日数を表示するプログラムを作成しなさい。2 月は 28 日とします。
     // また、1 ～ 12 以外の数値が入力された場合に、“入力が間違っています”と表示しなさい。*switchを使う
+    System.out.println("月を入力してね。");
+    int month = Integer.parseInt(br.readLine());
+    if(month < 0 && month > 13) {
+        System.out.println("正しい月を入力してね");
+    }else {
+        switch(month) {
+            case 1:
+                System.out.println("31");
+                break;
+            case 2:
+                System.out.println("28");
+                break;
+            case 3:
+                System.out.println("31");
+                break;
+            case 4:
+                System.out.println("30");
+                break;
+            case 5:
+                System.out.println("31");
+                break;
+            case 6:
+                System.out.println("30");
+                break;
+            case 7:
+                System.out.println("31");
+                break;
+            case 8:
+                System.out.println("31");
+                break;
+            case 9:
+                System.out.println("30");
+                break;
+            case 10:
+                System.out.println("31");
+                break;
+            case 11:
+                System.out.println("30");
+                break;
+            case 12:
+                System.out.println("31");
+                break;
+        }
+    }
+
+    
+    //解答
+    switch( month )
+{
+    case 2:
+        System.out.println( "28日です" );
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        System.out.println( "30日です" );
+        break;
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        System.out.println( "31日です" );
+        break;
+    default:
+        System.out.println( "入力が間違っています" );
+        break;
+}
+
+
   }
 }
-// System.out.println
